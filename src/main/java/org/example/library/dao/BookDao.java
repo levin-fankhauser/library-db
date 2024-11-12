f1a419d52a5c4bd3cfeecd56d73d0724b75d9a42
levin-fankhauser <levin.fankhauser@gmx.ch>
2024-11-12 11:09:05 +0100

    Change Dao data

--- src/main/java/org/example/library/dao/BookDao.java
+++ src/main/java/org/example/library/dao/BookDao.java
@@ -8,8 +8,8 @@ import java.util.List;
 // Eine richtige Anbindung zur DB wird erst bei LB2 implementiert, da hier wie Besprochen noch keine DB benötigt wird, da der Dao in den Test gemockt wird. 
 public class BookDao {
 	private final List<Book> books = new ArrayList<>(List.of(
-			new Book("Title1", "Author1", 2000, "ISBN1", "Genre1", 300, "English", "Description1"),
-			new Book("Title2", "Author2", 2005, "ISBN2", "Genre2", 350, "German", "Description2")
+			new Book("Title_1", "Author1", 2000, "ISBN1", "Genre1", 300, "English", "Description1"),
+			new Book("Title_2", "Author2", 2005, "ISBN2", "Genre2", 350, "German", "Description2")
 	));
 
 	public List<Book> getAllBooks() {
